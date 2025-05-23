90254598182024c1a3fd1f54257b83036ae3ea12
zinzo <zinzoddari@gmail.com>
2025-05-23 21:20:47 +0900

    feat: JPA Auditing 추가

---
+++ src/main/java/com/ddd/api/infra/orm/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.ddd.api.infra.orm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {}

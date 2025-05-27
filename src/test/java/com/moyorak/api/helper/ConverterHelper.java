e78a7d72942458fc0505518841b34aadc21baee4
zinzo <zinzoddari@gmail.com>
2025-05-28 00:46:54 +0900

    test: 샘플 컨트롤러 테스트 코드 추가

---
+++ src/test/java/com/moyorak/api/helper/ConverterHelper.java
@@ -0,0 +1,33 @@
+package com.moyorak.api.helper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.moyorak.api.global.domain.ListResponse;
+import java.util.List;
+
+public class ConverterHelper {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> T toDto(String request, Class<T> type) {
+        try {
+            return objectMapper.registerModule(new JavaTimeModule()).readValue(request, type);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> List<T> toDtoList(String input, Class<T> type) throws Exception {
+        return objectMapper.readValue(
+                input, objectMapper.getTypeFactory().constructCollectionType(List.class, type));
+    }
+
+    public static <T> ListResponse<T> toListResponse(String input, Class<T> type) throws Exception {
+        JavaType listResponseType =
+                objectMapper.getTypeFactory().constructParametricType(ListResponse.class, type);
+
+        return objectMapper.readValue(input, listResponseType);
+    }
+}

53c72ad721eca97d1b8118a6490dacce871f7694
zinzo <zinzoddari@gmail.com>
2025-06-08 21:08:15 +0900

    chore: #22 불필요한 csrf 설정 비활성화 처리

--- src/main/java/com/moyorak/config/security/SecurityConfig.java
+++ src/main/java/com/moyorak/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -34,10 +35,7 @@ class SecurityConfig {
                                                 HeadersConfigurer.FrameOptionsConfig
                                                         ::disable) // H2 콘솔 iframe 허용하기 위함
                                         .contentTypeOptions(Customizer.withDefaults()))
-                .csrf(
-                        csrf ->
-                                csrf.ignoringRequestMatchers(
-                                        "/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**"))
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .oauth2Login(
                         oauth ->

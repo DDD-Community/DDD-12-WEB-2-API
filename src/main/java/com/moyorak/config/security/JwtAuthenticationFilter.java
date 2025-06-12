36795088e09d16438a3bbd25480a23a7a72272fb
zinzo <zinzoddari@gmail.com>
2025-06-12 10:04:12 +0900

    refactor: #45 token 앞 prefix 제거하고 처리할 수 있도록 수정

--- src/main/java/com/moyorak/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/moyorak/config/security/JwtAuthenticationFilter.java
@@ -19,6 +19,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    public static final String JWT_TOKEN_PREFIX = "Bearer ";
+
     private final JwtTokenProvider jwtTokenProvider;
     private final AuthenticationEntryPoint authenticationEntryPoint;
 
@@ -27,12 +29,18 @@ class JwtAuthenticationFilter extends OncePerRequestFilter {
             HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        final String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         try {
-            if (ObjectUtils.isEmpty(token)) {
+            if (ObjectUtils.isEmpty(authorization)) {
                 throw new AuthenticationCredentialsNotFoundException("로그인 정보가 없습니다.");
             }
+
+            if (!authorization.startsWith(JWT_TOKEN_PREFIX)) {
+                throw new BadCredentialsException("잘못된 요청입니다.");
+            }
+
+            final String token = authorization.substring(JWT_TOKEN_PREFIX.length());
 
             if (!jwtTokenProvider.isValidToken(token)) {
                 throw new BadCredentialsException("잘못된 요청입니다.");
@@ -45,5 +53,15 @@ class JwtAuthenticationFilter extends OncePerRequestFilter {
         } catch (AuthenticationException e) {
             authenticationEntryPoint.commence(request, response, e);
         }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        final String uri = request.getRequestURI();
+
+        return uri.startsWith("/swagger-ui")
+                || uri.startsWith("/v3/api-docs")
+                || uri.startsWith("/h2-console")
+                || uri.equals("/favicon.ico");
     }
 }

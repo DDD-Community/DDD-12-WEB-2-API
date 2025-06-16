13530837368b558715e77949b8585167e67178a9
zinzo <zinzoddari@gmail.com>
2025-06-16 23:30:55 +0900

    feat: #50 로그인용 API 추가

--- src/main/java/com/moyorak/api/auth/dto/SignInRequest.java
+++ src/main/java/com/moyorak/api/auth/dto/SignInRequest.java
@@ -1,11 +1,8 @@
 package com.moyorak.api.auth.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Schema(title = "[로그인] 로그인 요청 DTO")
 public record SignInRequest(
-        @NotNull @Schema(description = "회원 고유 ID", example = "13") Long userId,
-        @NotBlank @Schema(description = "이메일", example = "honggildong@gmail.com") String email,
-        @NotBlank @Schema(description = "회원 이름", example = "홍길동") String name) {}
+        @NotNull @Schema(description = "회원 고유 ID", example = "13") Long userId) {}

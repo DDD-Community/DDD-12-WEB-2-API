8e6ae35b6450dccb9a45eba6bab268f1f2faefac
zinzo <zinzoddari@gmail.com>
2025-06-18 10:43:23 +0900

    chore: #56 저장 된 토큰과 불일치 할 경우 예외 발생시 시큐리티에서 처리하기 위해 BusinessException에서 시큐리티 예외로 변경

--- src/main/java/com/moyorak/api/auth/domain/InvalidTokenException.java
+++ src/main/java/com/moyorak/api/auth/domain/InvalidTokenException.java
@@ -1,8 +1,8 @@
 package com.moyorak.api.auth.domain;
 
-import com.moyorak.config.exception.BusinessException;
+import org.springframework.security.authentication.BadCredentialsException;
 
-public class InvalidTokenException extends BusinessException {
+public class InvalidTokenException extends BadCredentialsException {
 
     public InvalidTokenException() {
         super("유효하지 않은 로그인 정보입니다.");

d0abc3a72f224e1dc75368210821e72ddf9f7db3
zinzo <zinzoddari@gmail.com>
2025-06-19 11:24:05 +0900

    feat: #57 Refresh Token을 입력 받기 위한 Header 어노테이션 생성

---
+++ src/main/java/com/moyorak/config/web/RefreshTokenArgumentResolver.java
@@ -0,0 +1,33 @@
+package com.moyorak.config.web;
+
+import com.moyorak.api.auth.domain.RefreshToken;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+class RefreshTokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String REFRESH_TOKEN_HEADER = "X-REFRESH-TOKEN";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(RefreshToken.class)
+                && parameter.getParameterType().equals(String.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory)
+            throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        return request.getHeader(REFRESH_TOKEN_HEADER);
+    }
+}

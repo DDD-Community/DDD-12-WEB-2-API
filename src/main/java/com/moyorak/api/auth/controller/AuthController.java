06a6d0eef107c49574dbcdd822e276abdcd381c6
zinzo <zinzoddari@gmail.com>
2025-06-21 15:22:35 +0900

    refactor: #57 @RefreshToken 어노테이션 제거

--- src/main/java/com/moyorak/api/auth/controller/AuthController.java
+++ src/main/java/com/moyorak/api/auth/controller/AuthController.java
@@ -1,21 +1,25 @@
 package com.moyorak.api.auth.controller;
 
-import com.moyorak.api.auth.domain.RefreshToken;
 import com.moyorak.api.auth.domain.UserPrincipal;
 import com.moyorak.api.auth.dto.RefreshResponse;
 import com.moyorak.api.auth.dto.SignInRequest;
 import com.moyorak.api.auth.dto.SignInResponse;
 import com.moyorak.api.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -39,7 +43,11 @@ class AuthController {
 
     @PostMapping("/refresh")
     @Operation(summary = "[토큰 갱신] 토큰 갱신", description = "액세스 토큰을 재발급 합니다.")
-    public RefreshResponse refresh(@RefreshToken final String refreshToken) {
+    public RefreshResponse refresh(
+            @Parameter(description = "리프레시 토큰", required = true)
+                    @RequestHeader(name = "X-REFRESH-TOKEN")
+                    @NotBlank
+                    final String refreshToken) {
         return authService.refresh(refreshToken);
     }
 }

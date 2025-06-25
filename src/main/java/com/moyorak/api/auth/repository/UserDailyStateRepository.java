a161907ea7374dd3f0bd498eb5056f6dbe501713
Jiwon-cho <gwon188@gmail.com>
2025-06-25 22:19:12 +0900

    chore: #92 레퍼지토리 어노테이션 제거

--- src/main/java/com/moyorak/api/auth/repository/UserDailyStateRepository.java
+++ src/main/java/com/moyorak/api/auth/repository/UserDailyStateRepository.java
@@ -4,9 +4,7 @@ import com.moyorak.api.auth.domain.UserDailyState;
 import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserDailyStateRepository extends CrudRepository<UserDailyState, Long> {
     Optional<UserDailyState> findByUserIdAndRecordDate(Long userId, LocalDate recordDate);
 }

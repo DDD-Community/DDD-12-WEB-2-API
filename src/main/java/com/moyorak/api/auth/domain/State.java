f862a3fda48bcf537bb9dcf96cfd28ce42f66c79
Jiwon-cho <gwon188@gmail.com>
2025-06-25 23:50:42 +0900

    refactor: #92 상태 널 체크 로직 추가

--- src/main/java/com/moyorak/api/auth/domain/State.java
+++ src/main/java/com/moyorak/api/auth/domain/State.java
@@ -1,6 +1,24 @@
 package com.moyorak.api.auth.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.springframework.util.StringUtils;
+
 public enum State {
     ON,
-    OFF
+    OFF;
+
+    @JsonCreator
+    public static State from(final String input) {
+        if (!StringUtils.hasText(input)) {
+            return null;
+        }
+
+        for (State state : State.values()) {
+            if (input.equals(state.name())) {
+                return state;
+            }
+        }
+
+        return null;
+    }
 }

811ffb7b57052264f3d9ea141cbaa5b0e020d6e8
Jiwon-cho <gwon188@gmail.com>
2025-06-26 20:44:40 +0900

    refactor: #92 토글 업데이트로 인한 jsoncreator 제거

--- src/main/java/com/moyorak/api/auth/domain/State.java
+++ src/main/java/com/moyorak/api/auth/domain/State.java
@@ -1,24 +1,6 @@
 package com.moyorak.api.auth.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import org.springframework.util.StringUtils;
-
 public enum State {
     ON,
-    OFF;
-
-    @JsonCreator
-    public static State from(final String input) {
-        if (!StringUtils.hasText(input)) {
-            return null;
-        }
-
-        for (State state : State.values()) {
-            if (input.equals(state.name())) {
-                return state;
-            }
-        }
-
-        return null;
-    }
+    OFF
 }

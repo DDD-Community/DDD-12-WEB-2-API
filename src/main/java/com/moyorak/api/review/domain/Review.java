e444db9847b320ca10a9164a57850e40bee26a4a
Jiwon-cho <gwon188@gmail.com>
2025-07-02 23:41:57 +0900

    feat: #124 리뷰 연관관계 설정

--- src/main/java/com/moyorak/api/review/domain/Review.java
+++ src/main/java/com/moyorak/api/review/domain/Review.java
@@ -1,14 +1,23 @@
-package com.moyorak.api.review;
+package com.moyorak.api.review.domain;
 
+import com.moyorak.api.auth.domain.User;
+import com.moyorak.api.team.domain.TeamRestaurant;
 import com.moyorak.infra.orm.AuditInformation;
 import com.moyorak.infra.orm.BooleanYnConverter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,32 +33,39 @@ public class Review extends AuditInformation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @Comment("평점")
     @Column(name = "score", nullable = false)
     private Integer score;
 
+    @NotNull
     @Comment("음식 나오는 시간")
     @Column(name = "serving_time", nullable = false)
-    private Integer serving_time;
+    private Integer servingTime;
 
+    @NotNull
     @Comment("대기 시간")
     @Column(name = "waiting_time", nullable = false)
-    private Integer waiting_time;
+    private Integer waitingTime;
 
     @Comment("추가 텍스트")
     @Column(name = "extra_text", nullable = false, columnDefinition = "varchar(512)")
-    private String extra_text;
+    private String extraText;
 
     @Comment("사용 여부")
     @Convert(converter = BooleanYnConverter.class)
     @Column(name = "use_yn", nullable = false, columnDefinition = "char(1)")
     private boolean use = true;
 
-    @Comment("사용자 고유 ID")
-    @Column(name = "member_id", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private User user;
 
-    @Comment("식당 고유 ID")
-    @Column(name = "restaurant_id", nullable = false)
-    private Long restaurantId;
+    @Comment("팀 맛집 고유 ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_restaurant_id", nullable = false)
+    private TeamRestaurant teamRestaurant;
+
+    @OneToMany(mappedBy = "review_id")
+    private List<ReviewPhoto> reviewPhotos = new ArrayList<>();
 }

6592672b0f2694e147d007e472006e0e83a4f3dd
Jiwon-cho <gwon188@gmail.com>
2025-07-15 14:55:27 +0900

    refactor: #148 누락된 변수 설명 추가

--- src/main/java/com/moyorak/api/review/dto/PhotoPath.java
+++ src/main/java/com/moyorak/api/review/dto/PhotoPath.java
@@ -1,3 +1,6 @@
 package com.moyorak.api.review.dto;
 
-public record PhotoPath(String path) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record PhotoPath(
+        @Schema(description = "사진 경로", example = "https://somepath/review.jpg") String path) {}
